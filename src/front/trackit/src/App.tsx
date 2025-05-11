5a4cdc4511619ca084ce896e01762fc3f2493e5b
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-11 17:45:20 -0300

    teste husky5

--- src/front/trackit/src/App.tsx
+++ src/front/trackit/src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 
-export function App() {
+xport function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-4">
       <Card className="p-4 w-[400px]">
