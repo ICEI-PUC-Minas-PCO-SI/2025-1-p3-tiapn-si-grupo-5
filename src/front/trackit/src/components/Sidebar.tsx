6cf3bea1611baeca3d7d76fcb135dd7ea0596e15
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-17 13:23:28 -0300

    frontend estavel e pronto

--- src/front/trackit/src/components/Sidebar.tsx
+++ src/front/trackit/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import Logo from "../assets/TrackIt_Logo.svg";
+import { SidebarVariant } from "./SidebarVariant";
 
 {
   /*Variáveis que serão consumidos da API*/
@@ -10,15 +10,14 @@ import Logo from "../assets/TrackIt_Logo.svg";
 const userPicture = null;
 const userName = "usuário";
 const userManagement = "gerência";
+const userType = 1;
 
 export function Sidebar() {
   const [collapsed, setCollapsed] = useState(false);
-
   return (
     <div
-      className={`flex-col gap-6 min-h-screen py-4 px-2 bg-slate-200 inline-flex justify-start items-start transition-all duration-400 relative ${
-        collapsed ? "w-22" : "w-44 px-8"
-      }`}
+      className={`flex-col gap-6 py-5.5 min-h-screen bg-slate-200 inline-flex justify-start items-start transition-all duration-400 relative ${collapsed ? "w-22" : "w-44 px-4"
+        }`}
     >
       <button
         className="absolute left-full top-6 -translate-x-1/2 z-10 flex items-center justify-center rounded-full bg-sky-700 shadow-lg cursor-pointer"
@@ -33,21 +32,11 @@ export function Sidebar() {
           <FaChevronLeft size="0.8rem" className="text-slate-100" />
         )}
       </button>
-
-      <div className="w-full flex items-center justify-center mb-4 min-h-[3.5rem]">
-        {collapsed ? (
-          <img
-            src={Logo}
-            alt="TrackIT Logo"
-            className="transition-all duration-300 w-16"
-          />
-        ) : (
+      <div className="w-full flex items-center justify-center">
           <span className="text-center button-base text-2xl font-bold tracking-wide w-full">
             TrackIT
           </span>
-        )}
       </div>
-      {/* Usuário e Gerência ou só foto */}
       <div className="w-full flex justify-center items-center transition-all duration-300">
         {collapsed ? (
           <span className="w-[36px] h-[36px] rounded-full overflow-hidden flex items-center justify-center">
@@ -58,7 +47,7 @@ export function Sidebar() {
             )}
           </span>
         ) : (
-          <div className="inline-flex self-stretch justify-start gap-2.5 w-full">
+          <div className="inline-flex self-stretch justify-evenly gap-2.5 w-full">
             <span className="w-[36px] h-[36px] rounded-full overflow-hidden flex items-center justify-center">
               {userPicture ? (
                 <img src={userPicture} />
@@ -73,6 +62,7 @@ export function Sidebar() {
           </div>
         )}
       </div>
+      <SidebarVariant userType={userType} isCollapsed={collapsed} />
     </div>
   );
 }
--- src/front/trackit/src/components/SidebarVariant.tsx
+++ src/front/trackit/src/components/SidebarVariant.tsx
@@ -1,5 +1,171 @@
+import {
+    HandPointingIcon,
+    PlusCircleIcon,
+    ClockCountdownIcon,
+    GearIcon,
+    ChartBarIcon,
+    FadersIcon,
+    CheckCircleIcon,
+    CheckerboardIcon,
+    BuildingOfficeIcon,
+    UserCircleGearIcon,
+    FolderOpenIcon
+} from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
+interface SidebarVariantProps {
+    userType: number;
+    isCollapsed: boolean;
+}
 
-export function Navigation () {
-    
+export function SidebarVariant({ userType, isCollapsed }: SidebarVariantProps) {
+    const navClass = `flex items-center ${isCollapsed ? "justify-center" : "justify-between"
+        }`;
+
+    if (userType === 1) {
+        return (
+            <div className="flex flex-col gap-6 w-full">
+                <Link to="open-ticket" className="block">
+                    <nav className={navClass}>
+                        <PlusCircleIcon size={30} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">abrir chamado</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="see-tickets" className="block">
+                    <nav className={navClass}>
+                        <ClockCountdownIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">ver chamados</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="settings" className="block">
+                    <nav className={navClass}>
+                        <GearIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">configurações</span>
+                        )}
+                    </nav>
+                </Link>
+            </div>
+        );
+    }
+    if (userType === 2) {
+        return (
+            <div className="flex flex-col gap-6 w-full">
+                <Link to="choose-ticket" className="block">
+                    <nav className={navClass}>
+                        <HandPointingIcon size={30} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">pegar chamado</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="see-tickets" className="block">
+                    <nav className={navClass}>
+                        <ClockCountdownIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">ver chamados</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="settings" className="block">
+                    <nav className={navClass}>
+                        <ChartBarIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">desempenho</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="settings" className="block">
+                    <nav className={navClass}>
+                        <GearIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">configurações</span>
+                        )}
+                    </nav>
+                </Link>
+            </div>
+        );
+    }
+    if (userType === 3) {
+        return (
+            <div className="flex flex-col gap-6 w-full">
+                <Link to="dashbord" className="block">
+                    <nav className={navClass}>
+                        <ChartBarIcon size={30} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">dashbord</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="params/priority" className="block">
+                    <nav className={navClass}>
+                        <FadersIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">prioridades</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="params/status" className="block">
+                    <nav className={navClass}>
+                        <CheckCircleIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">status</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="params/type" className="block">
+                    <nav className={navClass}>
+                        <CheckerboardIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">tipo</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="params/managment" className="block">
+                    <nav className={navClass}>
+                        <BuildingOfficeIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">gerências</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="params/users" className="block">
+                    <nav className={navClass}>
+                        <UserCircleGearIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">usuários</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="tickets/opened" className="block">
+                    <nav className={navClass}>
+                        <FolderOpenIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">em aberto</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="tickets/opened" className="block">
+                    <nav className={navClass}>
+                        <HandPointingIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">atribuídos</span>
+                        )}
+                    </nav>
+                </Link>
+                <Link to="settings" className="block">
+                    <nav className={navClass}>
+                        <GearIcon size={32} />
+                        {!isCollapsed && (
+                            <span className="menu-1 slate-800">configurações</span>
+                        )}
+                    </nav>
+                </Link>
+            </div>
+        );
+    }
 }
