eea5d2d4dabd1cbe50f19ef9ac1161cb7b22c8c8
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-23 14:42:39 -0300

    creating header

--- src/front/trackit/src/components/ui/button-variants.ts
+++ src/front/trackit/src/components/ui/button-variants.ts
@@ -1,25 +1,26 @@
 import { cva } from "class-variance-authority";
+import "../../styles/index.css";
 
 export const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive button-base",
   {
     variants: {
       variant: {
         default:
-          "bg-sky-700 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-sky-600 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-slate-800 transition-all duration-200 text-[1rem] text-slate-100 font-medium font-[Poppins] text-[1rem] font-semibold",
+          "bg-sky-700 text-slate-100 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-sky-600 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-slate-800 transition-all duration-200",
         outline:
-          "bg-transparent min-w-[7.75rem] h-[3rem] rounded-[0.375rem] border border-slate-700 cursor-pointer hover:shadow-md hover:shadow-slate-200 transition-all duration-300 ease-in-out text-[1rem] text-slate-700 font-[Poppins] text-[1rem] font-semibold",
+          "bg-transparent text-slate-700 min-w-[7.75rem] h-[3rem] rounded-[6px] border border-slate-300 cursor-pointer hover:shadow-md hover:shadow-slate-200 transition-all duration-300 ease-in-out focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         secondary:
-          "bg-slate-100 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-slate-200 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-slate-800 transition-all duration-200 text-[1rem] text-slate-700 font-[Poppins] text-[1rem] font-semibold",
+          "bg-slate-100 text-slate-700 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-slate-200 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-slate-800 transition-all duration-200",
         disabled:
-          "bg-sky-700 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-not-allowed text-[1rem] text-slate-100 font-[Poppins] text-[1rem] font-semibold opacity-50",
+          "bg-sky-700 text-slate-100 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-not-allowed opacity-50",
         delete:
-          "bg-red-600 min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-red-700 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-red-800 transition-all duration-200 text-[1rem] text-white font-[Poppins] text-[1rem] font-semibold",
+          "bg-red-600 text-white min-w-[7.75rem] h-[3rem] rounded-[0.375rem] cursor-pointer hover:bg-red-700 hover:shadow-[0px_0.125rem_0.5rem_0px] hover:shadow-red-800 transition-all duration-200",
       },
       size: {
-        default: "min-w-[7.75rem] h-[3rem]", // Default: 124px x 48px
-        sm: "h-[2rem] min-w-[7.75rem]", // Small: 32px height
-        icon: "min-w-[4.5rem] h-[3rem]", // Icon: 72px width
+        default: "min-w-[7.75rem] h-[3rem]",
+        sm: "h-[2.5rem] min-w-[7.75rem]",
+        icon: "min-w-[4.5rem] h-[3rem]",
       },
     },
     defaultVariants: {
--- src/front/trackit/src/pages/management-users/ManagementUsers.tsx
+++ src/front/trackit/src/pages/management-users/ManagementUsers.tsx
@@ -1,16 +1,24 @@
-import { Button } from "@/components/ui/button";
+/* import { Button } from "@/components/ui/button";
+import { Searchbar } from "@/components/SearchBar";
+import { FunnelIcon, FunnelSimpleIcon } from "@phosphor-icons/react"; */
 
 export function ManagementUsers() {
     return (
         <div className="">
             <h1 className="title-h1 d-block">Gerenciar Usuários</h1>
-            <div className="">
-                {/*TODO: Barra de pesquisa */}
+{/*             <div className="flex flex-gol justify-between">
+                <Searchbar />
                 <div className="flex w-[18rem] gap-6">
-                    <Button size={"sm"} variant={"outline"}>Filtrar</Button>
-                    <Button size={"sm"} variant={"outline"}>Pesquisar</Button>
+                    <Button size={"sm"} variant={"outline"}>
+                        <FunnelIcon size={20} className="text-slate-700" />
+                        Filtrar
+                        </Button>
+                    <Button size={"sm"} variant={"outline"}>
+                        <FunnelSimpleIcon size={20} className="text-slate-700" />
+                        Ordenar
+                        </Button>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
