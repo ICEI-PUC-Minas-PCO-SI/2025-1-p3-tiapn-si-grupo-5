55cbf5f601981cc56c9dc7b8e4775e701c3e0a21
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-29 14:19:31 -0300

    initializing structure

---
+++ src/front/trackit/src/pages/settings/Settings.tsx
@@ -0,0 +1,13 @@
+
+
+export function Settings() {
+    return (
+        <div className="flex flex-col">
+            <header>
+                <h1 className="title-h1">Configurações</h1>
+            </header>
+            <main>
+            </main>
+        </div>
+    )
+}
