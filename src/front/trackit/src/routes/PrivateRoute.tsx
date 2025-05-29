3840f19163f01e00e4198c1a0e53379e7e20c019
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-29 00:25:49 -0300

    fixing private routes

--- src/front/trackit/src/routes/PrivateRoute.tsx
+++ src/front/trackit/src/routes/PrivateRoute.tsx
@@ -1,11 +1,12 @@
 import { Navigate } from "react-router-dom";
 import { useUser } from "@/contexts/UserContext";
+import { DefaultSpinner } from "@/components/ui/spinner";
 
 export function PrivateRoute({ allowedTypes, children }: { allowedTypes: number[]; children: React.ReactNode }) {
     const { user, loading } = useUser();
 
     if (loading) {
-        // Pode exibir um spinner ou nada enquanto carrega
+        <DefaultSpinner />
         return null;
     }
 
@@ -14,7 +15,7 @@ export function PrivateRoute({ allowedTypes, children }: { allowedTypes: number[
     }
 
     if (!allowedTypes.includes(user.tipo ?? 0)) {
-        return <Navigate to="/user" />;
+        return <Navigate to="/" />;
     }
 
     return <>{children}</>;
