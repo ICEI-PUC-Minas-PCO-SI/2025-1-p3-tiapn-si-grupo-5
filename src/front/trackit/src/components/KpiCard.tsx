548d2235dbde2ee1c63b8e4514acbebf66e07e12
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-05-31 23:55:23 -0300

    finish kpi component

--- src/front/trackit/src/components/KpiCard.tsx
+++ src/front/trackit/src/components/KpiCard.tsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router";
 
-export default function KpiCard({ kpiTitle, kpiValue, kpiLink, kpiColor }: {
+export function KpiCard({ kpiTitle, kpiValue, kpiLink, kpiColor }: {
     kpiTitle: string;
     kpiValue: string | number;
     kpiLink?: string;
     kpiColor?: string;
 }) {
     return (
-        <div className="w-[40rem] flex items-center justify-start gap-4 p-4 bg-white rounded-lg shadow-md border border-slate-200">
+        <div className="w-[32rem] flex items-center justify-start gap-4 p-4 bg-white rounded-lg shadow-md border border-slate-200 relative">
             <span
-                className={`w-1 h-full rounded-sm ${kpiColor}`}
+                className="w-2 absolute top-0 left-0 bottom-0 rounded-l-md"
                 style={{ backgroundColor: kpiColor }}
             ></span>
-            <div className="flex flex-col items-start justify-start gap-2">
+            <div className="flex flex-col items-start justify-start gap-2 pl-4">
                 <h3 className="title-h3 text-slate-700">{kpiTitle}</h3>
-                <p className="title-h2 text-slate-800">{kpiValue}</p>
+                <p className="title-h2 text-slate-800"
+                >
+                    {kpiValue}
+                </p>
                 {kpiLink && (
                     <Link to={kpiLink} className="paragraph-base hover:underline">
                         Ver detalhes
@@ -23,5 +26,4 @@ export default function KpiCard({ kpiTitle, kpiValue, kpiLink, kpiColor }: {
             </div>
         </div>
     );
-
 }
--- src/front/trackit/src/pages/dashbord/Dashbord.tsx
+++ src/front/trackit/src/pages/dashbord/Dashbord.tsx
@@ -1,5 +1,35 @@
+import { KpiCard } from "@/components/KpiCard"
+
 export function Dashboard() {
     return (
-        <h1 className="title-h1 text-slate-950">Dashboard</h1>
+        <div className="flex flex-col w-full h-full gap-4 p-4">
+            <h1 className="title-h1 text-slate-950">Dashboard</h1>
+            <div className="flex flex-wrap gap-8">
+                <KpiCard
+                    kpiTitle="Chamados resolvidos"
+                    kpiValue="150"
+                    kpiLink="/"
+                    kpiColor="#46be00"
+                />
+                <KpiCard
+                    kpiTitle="Chamados resolvidos"
+                    kpiValue="150"
+                    kpiLink="/"
+                    kpiColor="#46be00"
+                />
+                <KpiCard
+                    kpiTitle="Chamados resolvidos"
+                    kpiValue="150"
+                    kpiLink="/"
+                    kpiColor="#46be00"
+                />
+                <KpiCard
+                    kpiTitle="Chamados resolvidos"
+                    kpiValue="150"
+                    kpiLink="/"
+                    kpiColor="#46be00"
+                />
+            </div>
+        </div>
     )
 }
