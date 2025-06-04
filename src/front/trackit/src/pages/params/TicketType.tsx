60096c6d978cd9fdddc93162e0e805c577f69df1
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-06-04 11:00:03 -0300

    adding frontend

--- src/front/trackit/src/pages/params/TicketType.tsx
+++ src/front/trackit/src/pages/params/TicketType.tsx
@@ -1,5 +1,22 @@
-export function TicketType () {
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { TicketTypeParams } from "@/components/TicketTypeParams";
+
+export function TicketType() {
+    const [isAdding, setIsAdding] = useState(false);
+
     return (
-        <h1>Hello World!</h1>
-    )
+        <div className="flex flex-col gap-6">
+            <h1 className="title-h1 text-slate-950">Gerenciar Parâmetros</h1>
+            <div className="border-2 p-4 rounded mb-6">
+                <div className="flex justify-between">
+                    <h2 className="title-h2 text-slate-700">Editar tipos de demanda</h2>
+                    <Button className="w-38 mr-6" onClick={() => setIsAdding(true)}>
+                        + Adicionar
+                    </Button>
+                </div>
+                <TicketTypeParams isAdding={isAdding} setIsAdding={setIsAdding} />
+            </div>
+        </div>
+    );
 }
