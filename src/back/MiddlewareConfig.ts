97b4a8af4177219ae079b346153dfd8c456bd0c0
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-06-22 19:07:24 -0300

    backend -> frontend url changed

--- src/back/MiddlewareConfig.ts
+++ src/back/MiddlewareConfig.ts
@@ -7,7 +7,7 @@ export class MiddlewareConfig {
     public static register(app: Express) {
         app.use(express.json());
         app.use(cors({
-            origin: process.env.FRONTEND_URL,
+            origin: "https://trackit-front.onrender.com",
             credentials: true,
             methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
             allowedHeaders: ["Content-Type", "Authorization"]
