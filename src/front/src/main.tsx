ce41c124f01865bbb8b42c3c3b9e421aad1516fa
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-06-22 01:03:38 -0300

    api frontend url

--- src/front/src/main.tsx
+++ src/front/src/main.tsx
@@ -5,7 +5,6 @@ import { ThemeProvider } from "@/components/theme/theme-provider"
 
 export default App
 
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <UserProvider>
     <ThemeProvider storageKey="tackit-theme" defaultTheme="light">
@@ -13,3 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </ThemeProvider>
   </UserProvider>
 )
+
+console.log("API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
