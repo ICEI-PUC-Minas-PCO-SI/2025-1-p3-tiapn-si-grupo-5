a9ffeeb2be1ac296a56b3f9c85822363ef7f1a2c
Lucas Fernandes <lucas.nascimento.1529159@sga.pucminas.br>
2025-06-22 16:12:35 -0300

    cors test

--- src/back/MiddlewareConfig.ts
+++ src/back/MiddlewareConfig.ts
@@ -7,7 +7,7 @@ export class MiddlewareConfig {
     public static register(app: Express) {
         app.use(express.json());
         app.use(cors({
-            origin: process.env.FRONTEND_URL,
+            origin: "*",
             credentials: true
         }));
         app.use(cookieParser());
